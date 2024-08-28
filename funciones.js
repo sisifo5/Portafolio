0bb220a3fa9858e61e7d83a217752e7645df3b23
ArDroWeb <97034442+sisifo5@users.noreply.github.com>
2024-08-27 20:06:40 -0600

    z

--- funciones.js
+++ funciones.js
@@ -32,14 +32,14 @@ function decryptText() {
 
 
 //Las imagenes cambian  
-function verifyTrys() {//this function compares if the try are true and equal than secret number value
+function encryptText() {//this function compares if the try are true and equal than secret number value
 
     var img = document.getElementById("pic");//we GET the element my_pictures by id 
     img.src = "img/Lovepik_com-380494303-cartoon-cyberpunk-game-movie-character-portrait-character-modeling-data-model-characters.png";//use this route for change the pic 
 }
 
 
-function again(){// return to default settings and delete our data
+function decryptText(){// return to default settings and delete our data
     var img = document.getElementById("pic");
     img.src = "img/male.png";//return to defaul picture
 
